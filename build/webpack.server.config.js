8f1ef25f23ff244db845e8ef5b943f59ed0d7819
zhuyunhe <zhuyunhe@meituan.com>
2017-06-27 09:56:46 +0800

    zyh-init

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -27,7 +27,8 @@ module.exports = merge(base, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"server"'
-    }),
+    })
+    ,
     new VueSSRServerPlugin()
   ]
 })
